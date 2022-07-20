7e91706cf8cbe1be0b39816fd4fcd2be0a66c716
rakes <rakes@LAPTOP-D6TAF9DO>
2022-07-20 13:01:46 +0530

    commit 7

--- MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNgExample1Test.java
+++ MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNgExample1Test.java
@@ -12,7 +12,7 @@ public void practice1Test() {
 public void practice2Test() {
 	Reporter.log("Tyss--> test2",true);
 	//System.out.println("hiiii i am rakesh");
-	System.out.println("hiiii i am rajesh");
+	System.out.println("hiiii i am rajesh");//jenkin
 	
 	
 }
